bc4341f9b0684f00dcac11b335028522eed3b373
SAHILSOLANKI14 <sahil.solanki7622@gmail.com>
2024-11-21 19:05:14 +0530

    debounce on search

--- src/modules/WebSite/Header/productSearch.js
+++ src/modules/WebSite/Header/productSearch.js
@@ -1,26 +1,60 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Autocomplete from '@mui/material/Autocomplete';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { fetchProductRequest } from '../Product/Store/productAction'; 
-import noimg from '../../Categories/images/no_image.png';
-import { fetchproductsearch } from 'src/modules/Categories/API/ProductSearch';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Autocomplete from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchProductRequest } from "../Product/Store/productAction";
+import noimg from "../../Categories/images/no_image.png";
+import { fetchproductsearch } from "src/modules/Categories/API/ProductSearch";
+import SearchIcon from "@mui/icons-material/Search";
+import { InputAdornment } from "@mui/material";
+
+const debounce = (func, delay) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
+};
 
 const ProductSearch = () => {
   const [value, setValue] = React.useState(null);
-  const [inputValue, setInputValue] = React.useState('');
+  const [inputValue, setInputValue] = React.useState("");
+  const [debouncedValue, setDebouncedValue] = React.useState("");
   const [product, setProduct] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // Debounce input value changes
+  const debouncedInputHandler = React.useMemo(
+    () =>
+      debounce((newValue) => {
+        setDebouncedValue(newValue);
+      }, 300),
+    []
+  );
+
+  React.useEffect(() => {
+    debouncedInputHandler(inputValue);
+  }, [inputValue, debouncedInputHandler]);
+
+  // Fetch products when debounced value changes
   const FetchProducts = async () => {
-    if (inputValue) {
-      const response = await fetchproductsearch(inputValue); 
-      setProduct(response.data);
+    if (debouncedValue) {
+      setLoading(true);
+      try {
+        const response = await fetchproductsearch(debouncedValue);
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setLoading(false);
+      }
     } else {
       setProduct([]);
     }
@@ -28,10 +62,10 @@ const ProductSearch = () => {
 
   React.useEffect(() => {
     FetchProducts();
-  }, [inputValue]);
+  }, [debouncedValue]);
 
-  const handleProductClick = async (product) => {
-    dispatch(fetchProductRequest(product.code)); 
+  const handleProductClick = (product) => {
+    dispatch(fetchProductRequest(product.code));
     navigate(`/product/${product.slug}`);
   };
 
@@ -46,30 +80,47 @@ const ProductSearch = () => {
       inputValue={inputValue}
       onInputChange={(event, newInputValue) => setInputValue(newInputValue)}
       options={product}
-      getOptionLabel={(option) => option.name || ''}
+      getOptionLabel={(option) => option.name || ""}
       isOptionEqualToValue={(option, value) => option.id === value.id}
       noOptionsText="No products found"
+      freeSolo
+      loading={loading}
+      loadingText="Loading..."
       renderInput={(params) => (
         <TextField
           {...params}
           label="Search Products"
           placeholder="Type to search..."
-          sx={{ width: '400px' }}
+          sx={{ width: "400px" }}
+          InputProps={{
+            ...params.InputProps,
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <>
+                {loading ? <Typography variant="body2">Loading...</Typography> : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
         />
       )}
-      sx={{ width: '400px' }}
+      sx={{ width: "400px" }}
       renderOption={(props, option) => (
         <Box
           component="li"
           {...props}
           sx={{
-            display: 'flex',
-            alignItems: 'center',
+            display: "flex",
+            alignItems: "center",
             gap: 2,
-            padding: '8px',
+            padding: "8px",
           }}
         >
-          <Avatar src={option.image || noimg} alt={option.name} sx={{ width: 50, height: 50 }} />
+          <Avatar src={noimg ||  option.image } alt={option.name} sx={{ width: 50, height: 50 }} />
           <Box>
             <Typography variant="h5" fontWeight="600">
               {option.name}
